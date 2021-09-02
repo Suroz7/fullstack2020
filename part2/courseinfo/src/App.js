36a38df1ff88cee3d61caacefaf045a7c447031d
suroz <srz.grz@gmail.com>
2021-09-02 17:46:38 +0545

    2.1 completed

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Course from './Course'
+
+const App = () =>{
+  const course = {
+    id:1,
+    name:'Half Stack application development',
+    parts: [
+      {
+      name:'Fundamentals of React',
+      excercies : 10,
+      id:1
+    },
+      {
+      name:'Using props to pass data',
+      excercies:7,
+      id:2
+    },
+      {
+      name:'State of a component',
+      excercies:14,
+      id:3
+    }
+  ]
+  
+  
+  }
+
+  
+    return(
+      <div>
+      <Course course={course}/>
+      </div>
+    )
+  
+}
+
+
+export default App
