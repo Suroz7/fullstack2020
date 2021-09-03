6bb4470bc63dde8afd010e3bbad9adb6b98a2461
suroz <srz.grz@gmail.com>
2021-09-03 13:51:02 +0545

    part 2.13 completed

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,6 +4,7 @@ const App=()=>{
   const [countries,setCountries] = useState([])
   const [keyword,setKeyword] = useState('')
   const [filteredout,seFilteredOut] = useState([])
+  const [showdetail,setShowDetail] = useState('')
   useEffect(()=>{
     axios.get('https://restcountries.eu/rest/v2/all')
     .then(response=>{
@@ -29,12 +30,24 @@ const App=()=>{
          Capital {filteredout[0].capital}
          <h2>Languages</h2>
          <ul>
-           {filteredout[0].languages?filteredout[0].languages.map((fif)=><li>{fif.name}</li>):`No language avilable`}
+           {filteredout[0].languages?filteredout[0].languages.map((fif)=><li key={fif.name}>{fif.name}</li>):`No language avilable`}
          </ul>
          <h1>Flag</h1>
          <img src={filteredout[0].flag} alt="Contry flag"></img>
        </div>
-       :filteredout.map((filter)=><h4 key={filter.name}>{filter.name}</h4>)}
+       :filteredout.map((filter)=><div key={filter.name}><h4 >{filter.name}</h4> <button onClick={()=>setShowDetail(filter.name)}>Show</button>
+       {showdetail===filter.name?<div>
+         <h1>Name : {filter.name}</h1>
+         Capital ; {filter.capital}
+         <h2>Languages</h2>
+         <ul >
+           {filter.languages?filter.languages.map((fif)=><li key={fif.name}>{fif.name}</li>):`No language avilable`}
+         </ul>
+         <h1>Flag</h1>
+         <img src={filter.flag} alt="Contry flag"></img>
+
+       </div>:null}
+       </div>)}
      </div>
     </div>
     
