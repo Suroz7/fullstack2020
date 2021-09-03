707a2a9a7dced8912573f837889832dcc4fb17bf
suroz <srz.grz@gmail.com>
2021-09-03 11:18:50 +0545

    part 2.9 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,14 @@
 import React,{useState} from 'react';
 const App=()=>{
   const [persons,setPersons] = useState([
-    {name:"Suroz Suroz",number:'9800000000'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName,setNewname] = useState('')
   const [newNo,setNewNo] = useState('')
+  const [filter,setFilter] = useState('')
   const addPerson = (e)=>{
     e.preventDefault()
     const checker = persons.filter((person)=>person.name===newName)
@@ -23,9 +27,15 @@ const App=()=>{
   const handelNoChange=(e)=>{
     setNewNo(e.target.value)
   }
+  const handelFilterChange=(e)=>{
+    setFilter(e.target.value)
+  }
   return (
     <div>
       <h1>PhoneBook</h1>
+      filter shown with :<input onChange={handelFilterChange}/>
+      <br></br>
+      <h1>Add New No </h1>
       <form onSubmit={addPerson} >
         <div>
           Name:<input type="text"  name='name' onChange={handelNameChange}/>
@@ -43,7 +53,7 @@ const App=()=>{
         <h1>Numbers</h1>
       {persons.map((person)=>{
          return( <div key={person.name}>
-            {person.name} {person.number}<br></br>
+            {person.name.toLocaleUpperCase().includes(filter.toLocaleUpperCase())?`${person.name} ${person.number}`:`` } 
           </div>
          )
             }
