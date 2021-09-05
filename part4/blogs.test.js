4097558e2bd4b7613c778491ecb0117deab07335
suroz <srz.grz@gmail.com>
2021-09-05 18:49:35 +0545

    part 4.4 completed

--- part4/blogs.test.js
+++ part4/blogs.test.js
@@ -26,7 +26,25 @@ const blogs =[
         "likes":1
     }
 ]
+
     const result = listhelper.dummy(blogs)
     expect(result).toBe(1)
+    
 
+})
+describe('Total Likes',()=>{
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 25,
+          __v: 0
+        }
+      ]
+    test('when only one qual to thats like',()=>{
+        const result = listhelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(25)
+    })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,6 +1,10 @@
 const dummy = (blogs)=>{
     return 1
 }
+const totalLikes=(blogs)=>{
+    return blogs.reduce((initiallike,blogs)=>(initiallike +=blogs.likes),0)
+}
 module.exports={
-    dummy
+    dummy,
+    totalLikes
 }
