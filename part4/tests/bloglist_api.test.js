469414ff50f10908169888be5386a037ed9b141f
suroz <srz.grz@gmail.com>
2021-09-06 10:50:52 +0545

    part 4.9 completed

--- part4/tests/bloglist_api.test.js
+++ part4/tests/bloglist_api.test.js
@@ -14,6 +14,10 @@ test('Bolgs have length of 3',async ()=>{
    const response =  await api.get('/api/blogs')
     expect(response.body).toHaveLength(3)
 },100000)
+test('Blogs have unique identifier as id',async()=>{
+    const response = await api.get('/api/blogs')
+    expect(response.body[0]._id).toBeDefined()
+})
 afterAll(()=>{
     mongoose.connection.close()
 })
