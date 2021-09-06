7c4621cdffd59cdd3eb12fe51c9a60aedaaaeeb2
suroz <srz.grz@gmail.com>
2021-09-06 11:20:27 +0545

    part 4.10 completed

--- part4/tests/bloglist_api.test.js
+++ part4/tests/bloglist_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
 test('Blogs are returned as json',async ()=>{
     await api
     .get('/api/blogs')
@@ -9,15 +10,33 @@ test('Blogs are returned as json',async ()=>{
     .expect('Content-Type',/application\/json/)
 
 
-})
-test('Bolgs have length of 3',async ()=>{
+},10000)
+test('Bolgs have length of 9',async ()=>{
    const response =  await api.get('/api/blogs')
-    expect(response.body).toHaveLength(3)
+    expect(response.body).toHaveLength(9)
 },100000)
 test('Blogs have unique identifier as id',async()=>{
     const response = await api.get('/api/blogs')
     expect(response.body[0]._id).toBeDefined()
 })
+test('Addition of New Blog',async()=>{
+    const newBlog = {
+        title:"testing blog",
+        author:"tester",
+        url:"testing url",
+        likes:123
+    }
+    const initialBlogs = await api.get('/api/blogs')
+    const initialLength = initialBlogs.body.length
+    await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type',/application\/json/)
+    const newBlogs = await api.get('/api/blogs')
+    expect(newBlogs.body).toHaveLength(initialLength+1)
+    
+
+},10000)
 afterAll(()=>{
     mongoose.connection.close()
 })
