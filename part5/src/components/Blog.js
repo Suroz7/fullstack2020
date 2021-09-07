7e9e00f45d74afd387039cc56b9a5efa57b221b7
suroz <srz.grz@gmail.com>
2021-09-07 13:09:08 +0545

    part 5.7 completed

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,25 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+  const [showfulldetail,setshowfulldetail] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return(
+  <div style={blogStyle}>
+    {blog.title} {blog.author} <button onClick={()=>setshowfulldetail(!showfulldetail)}>{showfulldetail?'Hide':'Show'}</button>
+    <br/>
+    {showfulldetail &&
+    <div>
+    {blog.url}
+    <br/>
+    likes : {blog.like}  <button>Like</button>
+    </div>}
   </div>  
-)
+  )
+  }
 
 export default Blog
