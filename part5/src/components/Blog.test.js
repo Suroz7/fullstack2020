aa33d7bfd5257fb2c61fbf5a09ea1f01fb14e28c
suroz <srz.grz@gmail.com>
2021-09-07 15:51:08 +0545

    part 5.14 completed

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import Blog from './Blog'
 test('renders only title and author',() => {
     const blog = {
@@ -17,5 +17,27 @@ test('renders only title and author',() => {
     )
     expect(component.container).toHaveTextContent(
         'Testing Blog Tester'
+    )
+})
+test('view shows url and like',() => {
+    const blog = {
+        title:'Testing Blog',
+        author:'Tester',
+        url:'tester',
+        like:10
+    }
+    const fn = () => {
+        return  null
+    }
+    const component = render(
+        <Blog blog={blog} reloder={fn}/>
+    )
+    const btn = component.getByText('Show')
+    fireEvent.click(btn)
+    expect(component.container).toHaveTextContent(
+        'tester'
+    )
+    expect(component.container).toHaveTextContent(
+        '10'
     )
 })
