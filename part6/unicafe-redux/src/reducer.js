ff7a620768f7f49452bb3c6310fe1807366e8e93
suroz <srz.grz@gmail.com>
2021-09-08 12:40:55 +0545

    part 6.2 completed

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -13,11 +13,21 @@ const counterReducer = (state = initialState, action) => {
         good:state.good+1
       }
     case 'OK':
-      return state
+      return {
+        ...state,
+        ok:state.ok+1
+      }
     case 'BAD':
-      return state
+      return {
+        ...state,
+        bad:state.bad+1
+      }
     case 'ZERO':
-      return state
+      return {
+        bad:0,
+        good:0,
+        ok:0
+      }
     default: return state
   }
   
