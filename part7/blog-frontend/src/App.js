bd6e7d52e279a1fddb90e31c2f7cdd3e84bbeb84
suroz <srz.grz@gmail.com>
2021-09-17 16:14:53 +0545

    part 7.14 completed

--- part7/blog-frontend/src/App.js
+++ part7/blog-frontend/src/App.js
@@ -8,6 +8,8 @@ import { inits } from './reducers/blogReducer'
 import { adds, logouts } from './reducers/userReducer'
 import Users from './components/Users'
 import { setusers } from './reducers/usersReducer'
+import { Switch,Route, BrowserRouter as Router, Link } from 'react-router-dom'
+import User from './components/User'
 const App = (props) => {
   const dispatch = useDispatch()
   const [lstate,setLstate] = useState(false)
@@ -42,19 +44,28 @@ const App = (props) => {
 
     return (
       <div>
-        <Notification />
-        <p>{user} is Logged in</p>
-        <button id="logout"onClick={logOutHandler}>LogOut</button>
-        <br/>
-        <AddBlog />
-        <h2>blogs</h2>
-        {props.blogs.map(blog =>
-          <Blog key={blog._id} blog={blog}  />
-        )}
-        <h2>Users In the System</h2>
-        {props.users.map(user =>
-          <Users key={user._id} user = {user} />
-        )}
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Notification />
+              <p>{user} is Logged in</p>
+              <button id="logout"onClick={logOutHandler}>LogOut</button>
+              <br/>
+              <AddBlog />
+              <h2>blogs</h2>
+              {props.blogs.map(blog =>
+                <Blog key={blog._id} blog={blog}  />
+              )}
+              <h2>Users In the System</h2>
+              {props.users.map(user =>
+                <Link key={user._id} to ={`/user/${user._id}`}> <Users key={user._id } user={user}/> </Link>
+              )}
+            </Route>
+            <Route path='/user/:id'>
+              <User user={props.users}/>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     )
   }
