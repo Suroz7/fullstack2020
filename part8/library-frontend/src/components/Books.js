6dd74cf457cd40bf25c7b43790e6f2496e4ddac0
suroz <srz.grz@gmail.com>
2021-10-03 12:53:51 +0545

    part 8.20 completed

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { gql, useQuery} from '@apollo/client'
 const queryAllBooks = gql `
 query {
@@ -12,17 +12,30 @@ query {
   }
 }
 `
+const me = gql `
+query {
+  who{
+    username
+    favoriteGenre
+  }
+}`
 const Books = (props) => {
   const result = useQuery(queryAllBooks)
+  const userresult = useQuery(me)
+  const fg = !userresult.loading?userresult.data.who.favoriteGenre:'all'
+  useEffect(()=>{
+    if(!userresult.loading){
+      setGenre(fg)
+    }
+  },[userresult.loading,fg])
   const [genre,setGenre] = useState('all')
   const genres = ["refactoring", "agile", "patterns", "design", "crime", "classic","Gaming", "all"]
   if (!props.show) {
     return null
   }
-  if(result.loading){
+  if(result.loading || userresult.loading){
     return null
   }
-
   return (
     <div>
       <h2>books</h2>
@@ -53,10 +66,11 @@ const Books = (props) => {
             </tr>)}
         </tbody>
       </table>
-      <h2>Filter Books By {genre}</h2>
+      <h2>Filter Books By {genre===userresult.data.who.favoriteGenre? `favourite Genre ${genre}`:genre }</h2>
       {genres.map(g=>(
         <button id={g} disabled={genres===g?true:false} key={g} onClick={()=>setGenre(g)}>{g}</button>
       ))}
+      <button onClick={()=>setGenre(userresult.data.who.favoriteGenre)}>Favourite</button>
     </div>
   )
 }
